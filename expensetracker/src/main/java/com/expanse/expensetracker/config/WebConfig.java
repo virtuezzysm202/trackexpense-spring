7e08f2c29e7cad72f97190a32f861f7010a9a12d
virtuezzysm202 <162703883+virtuezzysm202@users.noreply.github.com>
2025-06-25 17:31:25 +0700

    Add CORS config to allow frontend access

---
+++ expensetracker/src/main/java/com/expanse/expensetracker/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.expanse.expensetracker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:7000") 
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true);
+    }
+}
